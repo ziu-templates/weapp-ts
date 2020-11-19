b4272a50936086d40345b648ecf7b7e2d0129c04
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-11-19 17:48:03 +0800

    feat: 修改json复制功能

--- template/scripts/lib/webpack/plugins/copyJsonPlugin.js
+++ template/scripts/lib/webpack/plugins/copyJsonPlugin.js
@@ -6,6 +6,20 @@ module.exports = function(entryJsonFiles, codePath) {
     return [];
   }
   let entryJson = Object.entries(entryJsonFiles).map(([page, pathurl]) => {
+    if (pathurl[0].includes("app.json")) {
+      return {
+        context: process.cwd(),
+        from: pathurl[0],
+        to: path.join(codePath, `${page}.json`),
+        transform() {
+          return JSON.stringify(
+            getAppJson({
+              autoImportAppConfigPath: conf.autoImportAppConfigPath,
+            }),
+          );
+        },
+      };
+    }
     return {
       context: process.cwd(),
       from: pathurl[0],
