5851b7972a354ec548ad599d18a6b2daa55fdfdd
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-12-24 16:39:37 +0800

    feat: 修改App格式

--- template/src/app.ts
+++ template/src/app.ts
@@ -2,9 +2,33 @@
  * 全局
  */
 import { updateApp } from 'miniapp-utils';
-import { getENV } from './utils/util';
-import app from './_app';
 
 updateApp();
-console.log(getENV());
-App(app);
+
+class AppOption implements IMippWeApp.ILifetime {
+  static getApp(opts?: IMippWeCommon.IGetAppOption): AppOption {
+    return getApp<AppOption>(opts);
+  }
+
+  private globalData = {};
+
+  onLaunch(opts: IMippWeApp.ILaunchOption): void {
+    console.log("onLaunch: ", opts);
+  }
+
+  onError(msg: string): void {
+    console.log("onError: ", msg);
+  }
+
+  onUnhandledRejection(msg: IMippWeApp.IOnUnhandledRejectionCallbackResult): void {
+    console.log("onUnhandledRejection: ", msg);
+  }
+
+  onPageNotFound(opts: IMippWeApp.IPageNotFoundOption): void {
+    console.log("onPageNotFound: ", opts);
+  }
+}
+
+export default AppOption;
+
+App(new AppOption());
