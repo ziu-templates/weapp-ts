8e67b55e28076e4f5aa35178840b23df7107cbac
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-12-24 16:36:10 +0800

    fix: 删除代码

--- template/src/_app.ts
+++ template/src/_app.ts
@@ -1,17 +1,3 @@
-export default {
-  onLaunch(): void {
-    console.log('onLaunch');
-  },
-  onShow(): void {
-    console.log('onShow');
-  },
-  onError(msg: string): void {
-    console.log(msg, ' -----> onError');
-  },
-  globalData: {
-    initData: null,
-  },
-};
 class AppOption implements IMippWeApp.ILifetime {
   static getApp(opts?: IMippWeCommon.IGetAppOption): AppOption {
     return getApp<AppOption>(opts);
