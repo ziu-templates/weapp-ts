40b3d4b1599d158d79392cbb887be70fb8237b2f
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-01-08 21:44:44 +0800

    feat: 增加utils

--- template/src/utils/util.ts
+++ template/src/utils/util.ts
@@ -1,12 +1,16 @@
 export const MODULE_NAME = 'util';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export function getENV(): any {
   let envData = {};
   try {
-    envData = process.env.ENV_DATA;
+    envData = process.env.ENV_DATA || {};
   } catch (e) {
     console.log(e);
   }
   return envData;
+}
+
+export function getEnvData(key: string): any {
+  return getENV()[key];
 }
