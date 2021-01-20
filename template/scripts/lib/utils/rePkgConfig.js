89f9786309e3c40ded7891631b6697be7494b1b3
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-01-20 10:51:41 +0800

    build: 修改构建

--- template/scripts/lib/utils/rePkgConfig.js
+++ template/scripts/lib/utils/rePkgConfig.js
@@ -1,37 +1,36 @@
-const merge = require('lodash.merge'),
-  { prjEnvComp } = require('./envCompare'),
-  envData = require('../envData/getEnvData'),
-  distPkg = require('../../etc/project.config');
-module.exports = function(content = '{}') {
-  let appid = '';
+const merge = require("lodash.merge"),
+  { prjEnvComp } = require("./envCompare"),
+  envData = require("../envData/getEnvData"),
+  distPkg = require("../../etc/project.config");
+module.exports = function (content = "{}") {
+  let appid = "";
   try {
-    appid = JSON.parse(envData || '{}').APP_ID || '';
-    content = JSON.stringify(merge(JSON.parse(content), appid ? { appid } : ''));
+    appid = JSON.parse(envData || "{}").APP_ID || "";
+    content = JSON.stringify(
+      merge(JSON.parse(content), appid ? { appid } : ""),
+    );
   } catch (e) {
     console.error(e);
   }
-  let pkgConf = '';
-  if (prjEnvComp('development')) {
-    try {
-      pkgConf = JSON.parse(content);
-    } catch (e) {
-      throw e;
-    }
-    if (pkgConf.projectname.includes(process.env.PRJ_ENV)) {
-      return JSON.stringify(pkgConf);
-    }
-    pkgConf.projectname = `${process.env.PRJ_ENV}-${pkgConf.projectname}`;
-    return JSON.stringify(pkgConf);
-  }
+  let pkgConf = "";
   try {
-    pkgConf = merge(JSON.parse(content), distPkg);
-    if (pkgConf.projectname.includes(process.env.PRJ_ENV)) {
-      return JSON.stringify(pkgConf);
-    }
-    pkgConf.projectname = `${process.env.PRJ_ENV}-${pkgConf.projectname.replace("development-", "")}`;
-    pkgConf = JSON.stringify(pkgConf);
+    pkgConf = JSON.parse(content);
   } catch (e) {
     throw e;
   }
-  return pkgConf;
-}
+  if (!pkgConf.projectname.includes(process.env.PRJ_ENV)) {
+    const projectName = prjEnvComp("development") ? pkgConf.projectname : pkgConf.projectname.replace("development-", "");
+    pkgConf.projectname = `${process.env.PRJ_ENV}-${projectName}`;
+  }
+
+  if (prjEnvComp("development")) {
+    return JSON.stringify(pkgConf);
+  }
+
+  try {
+    pkgConf = merge(pkgConf, distPkg);
+  } catch (e) {
+    throw e;
+  }
+  return JSON.stringify(pkgConf);
+};
